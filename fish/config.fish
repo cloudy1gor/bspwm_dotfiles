f727c0e2a2114a570bc3a67555a7d83ec1384f7f
cloudy1gor <savelyev7979@gmail.com>
2021-12-07 20:17:59 +0200

    fix: fish.config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 set fish_greeting ""
 
+set -x LESSHISTFILE -
+set -x LS_COLORS "*.jpeg=35:*.jpg=35:*.png=35:*.md=90:*.pdf=32:*.gif=35:*.tgz=31:*.mp3=92:*.tar.gz=31:*.zip=31:*.tex=95"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+
 set -gx TERM xterm-256color
 
 # theme
@@ -8,9 +12,15 @@ set -g fish_prompt_pwd_dir_length 1
 set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
+set -g theme_display_vi no
+set -g theme_display_date no
+set -g theme_nerd_fonts no
+
+set -g theme_powerline_fonts yes
+set -g fish_prompt_pwd_dir_length 0
 
 # aliases
-alias ls "ls -p -G"
+alias ls "ls --color=auto -p -G"
 alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
@@ -23,6 +33,7 @@ alias cp="cp -riv"
 alias mkdir="mkdir -vp"
 alias df="df -h"
 alias rm="rm -ir"
+alias c="clear"
 
 command -qv nvim && alias vim nvim
 
@@ -31,6 +42,16 @@ set -gx EDITOR nvim
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
+
+set FZF_DEFAULT_COMMAND "fd --type file --ignore-case --hidden --follow --exclude .git"
+set FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set FZF_ALT_C_COMMAND "fd --ignore-case --hidden -t d"
+set FZF_TMUX 1
+
+set TERM "kitty"
+
+set -x -g PIPENV_VENV_IN_PROJECT 1
+set -x -g PIPENV_TIMEOUT 3600
 
 # NodeJS
 set -gx PATH node_modules/.bin $PATH
