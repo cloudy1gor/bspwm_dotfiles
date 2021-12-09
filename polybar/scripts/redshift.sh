b09e2a594f2eff11b5525c8bd3a7166d30af88b8
cloudy1gor <savelyev7979@gmail.com>
2021-12-09 05:01:46 +0200

    fix: change polybar config and add wedder

---
+++ polybar/scripts/redshift.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+envFile=~/.config/polybar/scripts/env.sh
+changeValue=100
+
+changeMode() {
+  sed -i "s/REDSHIFT=$1/REDSHIFT=$2/g" $envFile 
+  REDSHIFT=$2
+  echo $REDSHIFT
+}
+
+changeTemp() {
+  if [ "$2" -gt 2700 ] && [ "$2" -lt 6450 ]
+  then
+    sed -i "s/REDSHIFT_TEMP=$1/REDSHIFT_TEMP=$2/g" $envFile 
+    redshift -P -O $((REDSHIFT_TEMP+changeValue))
+  fi
+}
+
+case $1 in 
+  toggle) 
+    if [ "$REDSHIFT" = on ];
+    then
+      changeMode "$REDSHIFT" off
+      redshift -x
+    else
+      changeMode "$REDSHIFT" on
+      redshift -O "$REDSHIFT_TEMP"
+    fi
+    ;;
+  increase)
+    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP+changeValue))
+    ;;
+  decrease)
+    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP-changeValue));
+    ;;
+  temperature)
+    case $REDSHIFT in
+      on)
+        printf "%dK" "$REDSHIFT_TEMP"
+        ;;
+      off)
+        printf "off"
+        ;;
+    esac
+    ;;
+esac
