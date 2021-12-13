494fd41770863377828b51401f54ed1dd8e4f6e1
cloudy1gor <savelyev7979@gmail.com>
2021-12-13 12:54:44 +0200

    fix: add wallpapers folder

--- bspwm/scripts/bg.sh
+++ bspwm/scripts/bg.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 while true; do
-        (( RANDOM%2 ==0 )) && feh --randomize --bg-fill ~/Изображения/2\ screens/* || feh --randomize --bg-fill ~/Изображения/*
-        sleep 600
+        feh --randomize --bg-fill ~/Изображения/*
+        sleep 15m
 done
