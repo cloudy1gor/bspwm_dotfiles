cb61e524063291625cb4f7876762a7dd71576c5b
cloudy1gor <savelyev7979@gmail.com>
2022-07-25 15:19:05 +0300

    feat: add settings to use second monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,3 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
 polybar top -c ~/.config/polybar/config.ini &
+
+if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+    polybar top_external &
+fi
