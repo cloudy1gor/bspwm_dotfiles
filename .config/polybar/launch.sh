b2f67c27f29766a8d159d065916a0bba66086e8c
cloudy1gor <savelyev7979@gmail.com>
2022-07-26 22:36:14 +0300

    feat: change bspwm.rc fish plugins and polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,5 +10,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar top -c ~/.config/polybar/config.ini &
 
 if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+    polybar -r top -c ~/.config/polybar/config.ini &
     polybar top_external &
 fi
