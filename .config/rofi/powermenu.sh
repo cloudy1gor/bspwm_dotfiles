3ec7f44b48a506b65ed7605697a545e396624f3f
cloudy1gor <savelyev7979@gmail.com>
2022-08-06 22:24:23 +0300

    feat: add new script to rofi and change old scripts

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/env bash
+DIR="$HOME/.config/rofi"
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-dir="$HOME/.config/rofi/"
 rofi_command="rofi -theme $dir/powermenu.rasi"
 
 uptime=$(uptime -p | sed -e 's/up //g')
@@ -20,40 +14,52 @@ logout="<"
 # Variable passed to rofi
 options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 
-chosen="$(echo -e "$options" | $rofi_command -p "$uptime" -dmenu -selected-row 0)"
+chosen="$(echo -e "$options" | $rofi_command -p "UP - $uptime" -dmenu -selected-row 2)"
 case $chosen in
-$shutdown)
-  systemctl poweroff
-  ;;
-$reboot)
-  systemctl reboot
-  ;;
-$lock)
-  if [[ -f /usr/bin/i3lock ]]; then
-    /home/$USER/.config/bspwm/scripts/i3lock-fancy/i3lock-fancy.sh
-  elif [[ -f /usr/bin/betterlockscreen ]]; then
-    betterlockscreen -l
-  fi
-  ;;
-$suspend)
-  ans=$(confirm_exit &)
-  if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-    mpc -q pause
-    amixer set Master mute
-    systemctl suspend
-  elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-    exit 0
-  else
-    msg
-  fi
-  ;;
-$logout)
-  if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-    openbox --exit
-  elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-    bspc quit
-  elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-    i3-msg exit
-  fi
-  ;;
+    $shutdown)
+    ans=$($HOME/.config/rofi/confirm.sh)
+    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]]; then
+        systemctl poweroff
+    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]]; then
+        exit
+        else
+            rofi -theme ~/.config/rofi/askpass.rasi
+        fi
+        ;;
+    $reboot)
+    ans=$($HOME/.config/rofi/confirm.sh)
+    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]]; then
+        systemctl reboot
+    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]]; then
+        exit
+        else
+            rofi -theme ~/.config/rofi/askpass.rasi
+        fi
+        ;;
+    $lock)
+        ~/.config/bspwm/scripts/i3lock-fancy/i3lock-fancy.sh
+        ;;
+    $suspend)
+    ans=$($HOME/.config/rofi/confirm.sh)
+    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]]; then
+        mpc -q pause
+        amixer set Master mute
+        betterlockscreen --suspend
+        #betterlockscreen --suspend -u /usr/share/backgrounds/"$Background"
+    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]]; then
+        exit
+        else
+            rofi -theme ~/.config/rofi/askpass.rasi
+        fi
+        ;;
+    $logout)
+    ans=$($HOME/.config/rofi/confirm.sh)
+    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]]; then
+        bspc quit
+    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]]; then
+        exit
+        else
+            rofi -theme ~/.config/rofi/askpass.rasi
+        fi
+        ;;
 esac
