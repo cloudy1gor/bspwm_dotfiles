f988e8b3ddaa4dc1d8a5692d5bf5ddf2d12e2a10
cloudy1gor <savelyev7979@gmail.com>
2022-08-06 22:21:13 +0300

    fix: change configuration files

--- .config/bspwm/scripts/bspfloat.sh
+++ .config/bspwm/scripts/bspfloat.sh
@@ -3,8 +3,8 @@
 ## Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
 ## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
 
-## All windows are floating on desktop 1
-FLOATING_DESKTOP_ID=$(bspc query -D -d '^1')
+## All windows are floating on desktop 5
+FLOATING_DESKTOP_ID=$(bspc query -D -d '^5')
 
 bspc subscribe node_add | while read -a msg ; do
    desk_id=${msg[2]}
