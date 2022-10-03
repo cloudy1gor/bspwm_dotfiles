d9d0641e27e2ddb93db07056344963655a04eca0
cloudy1gor <savelyev7979@gmail.com>
2022-10-03 19:09:43 +0300

    fix: change config

--- .config/bspwm/scripts/autostart.sh
+++ .config/bspwm/scripts/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a restart=( blueberry mictray copyq plank flameshot telegram-desktop -startintray )
+declare -a restart=( blueberry mictray kuro copyq plank flameshot telegram-desktop -startintray )
 for i in "${restart[@]}"; do
     pgrep -x "$i" | xargs kill
     sleep 0.5
